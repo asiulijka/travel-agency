6a338cbaf85ce669ce4246ae47ad49e32f133704
asiulijka <j.m.h.zielinska@gmail.com>
2021-07-31 21:43:09 +1000

    Change OrderForm - task 19.4

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,16 +5,54 @@ import {Row, Col} from 'react-flexbox-grid';
 import PropTypes from 'prop-types';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import settings from '../../../data/settings';
+import Button from '../../common/Button/Button';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+
+const sendOrder = (options, tripCost, tripId, tripName, tripCountry) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    tripId, 
+    tripName, 
+    tripCountry,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 class OrderForm extends React.Component {
   static propTypes = {
     tripCost: PropTypes.string,
     options: PropTypes.object,
     setOrderOption: PropTypes.func,
+    tripId: PropTypes.string, 
+    tripName: PropTypes.string, 
+    tripCountry: PropTypes.string, 
+    isReadyToSend: PropTypes.func,
   }
 
   render(){
-    const {tripCost, options, setOrderOption} = this.props;
+    const {tripCost, options, setOrderOption, tripId, tripName, tripCountry, isReadyToSend} = this.props;
     return (
       <Row>
         
@@ -28,6 +66,14 @@ class OrderForm extends React.Component {
         <Col xs={12}>
           <OrderSummary tripCost={tripCost} options={options} />
         </Col>
+
+        <Button 
+          onClick={() => sendOrder(options, tripCost, tripId, tripName, tripCountry)}
+          disabled={!isReadyToSend}
+        >
+          Order now!
+        </Button>
+
       </Row>
     );
   }
--- src/components/features/OrderForm/OrderFormContainer.js
+++ src/components/features/OrderForm/OrderFormContainer.js
@@ -1,9 +1,11 @@
 import {connect} from 'react-redux';
 import OrderForm from './OrderForm';
 import {getOrderOptions, setOrderOption} from '../../../redux/orderRedux';
+// import OrderOptionText from '../OrderOption/OrderOptionText';
 
 const mapStateToProps = state => ({
   options: getOrderOptions(state),
+  isReadyToSend: state.order.options.name != '' && state.order.options.contact != '',
 });
 
 const mapDispatchToProps = dispatch => ({
